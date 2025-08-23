be7ce5ec05796d9420ed1b4518922dcdae287606
john hinch <johnhnch864@gmail.com>
2025-08-23 17:03:32 +0100

    Test Summary - Date Filter

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -27,3 +27,11 @@ def test_summary_valid_user():
     assert data["min_amount"] == 100.0
     assert data["max_amount"] == 200.0
     assert data["mean_amount"] == 150.0
+
+def test_summary_date_filter():
+    response = client.get("/summary/101?start_date=2024-01-02")
+    assert response.status_code==200
+    data = response.json()
+    assert data["min_amount"] == 200.0
+    assert data["max_amount"] == 200.0
+    assert data["mean_amount"] == 200.0
