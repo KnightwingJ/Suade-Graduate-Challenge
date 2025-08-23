17246d061956684c7a716edf63a61151a5716030
john hinch <johnhnch864@gmail.com>
2025-08-23 15:50:08 +0100

    Test Upload Success

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -12,3 +12,17 @@ def test_upload_invalid_format():
     )
     assert response.status_code == 400
     assert response.json()["detail"] == "Invalid file format. Must be .csv file"
+
+def test_upload_valid_csv():
+    csv_content = (
+        "transaction_id,user_id,product_id,timestamp,transaction_amount\n"
+        "1,101,501,2024-01-01 10:00:00,100.0\n"
+        "2,101,502,2024-01-02 12:00:00,200.0\n"
+    )
+    response = client.post(
+        "/upload/",
+        files={"file":("test.csv",io.BytesIO(csv_content.encode()),"text/csv")},
+    )
+    assert response.status_code == 200
+    assert response.json()["message"] == "File Uploaded and Processed Successfully"
+    
